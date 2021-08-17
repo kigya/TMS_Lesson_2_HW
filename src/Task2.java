0730b381d4ef07068018077e8fba3a2933acca78
kigya <kirillborichevskiy@gmail.com>
2021-08-17 19:20:48 +0300

    Add Task2.java

---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int binary = 0b10111;
+        int octal = 032;
+        int decimal = 15;
+        int hexadecimal = 0xA4;
+        System.out.printf("Binary: %d%nOctal: %d%nDecimal: %d%nHexadecimal: %d", binary, octal, decimal, hexadecimal);
+    }
+}
