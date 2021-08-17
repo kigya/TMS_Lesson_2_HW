9580fe3fd8f62241d61a8f695266408cee91b690
kigya <kirillborichevskiy@gmail.com>
2021-08-17 20:09:10 +0300

    Add Task6.java

---
+++ src/Task6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * 6. Создать класс.
+ * В методе main получить введенное с консоли слово и вывести на консоль выражение:
+ * введенное слово "и_тут_в_кавычках_должно_быть_введенное_слово".
+ */
+public class Task6 {
+    /**
+     * Main method that prints the first word from console
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+    }
+}
