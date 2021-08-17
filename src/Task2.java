bcb66dea3fe7c7f19389755acc4552bf4035ddd1
kigya <kirillborichevskiy@gmail.com>
2021-08-17 19:29:33 +0300

    Update comments in Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,13 @@
+/**
+ * 2. Создать класс.
+ * В методе main объявить четрые переменные типа int и присвоить им произвольные числовые значения.
+ * Первая переменная должна быть в двоичном формате, вторая - в восьмеричном, третья - в десячином, четветая в шестнадцатиричном.
+ * Вывести результат на консоль.
+ */
 public class Task2 {
+    /**
+     * Main method to output the values of four variables in different number systems to the console
+     */
     public static void main(String[] args) {
         int binary = 0b10111;
         int octal = 032;
